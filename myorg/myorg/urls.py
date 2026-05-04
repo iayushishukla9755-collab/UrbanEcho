5370c109a7647e3b395f770ca4192882241a0933
iayushishukla9755 <iayushishukla9755@gmail.com>
2026-05-04 14:01:38 +0530

    urls.py missing paths

--- myorg/myorg/urls.py
+++ myorg/myorg/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -22,4 +24,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('', include('public_issue.urls')),
 ]
+
+# Serve media files in both development and production
+if settings.DEBUG or True:  # Serve media files in production too
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
